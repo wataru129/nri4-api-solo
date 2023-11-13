fa81e1dd47f7c87debd8df93a5cedc73a76d0fe7
wataru129 <w0129akjm@gmail.com>
2023-11-13 12:32:06 +0900

    migration/seedの作成完了

--- db/migrations/20231113023242_init.js
+++ db/migrations/20231113023242_init.js
@@ -3,17 +3,26 @@
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
-  return knex.schema.hasTable('テーブル名')
+  return knex.schema.hasTable('snack')
     .then(function(exists) {
       if (!exists) {
            // 接続先のスキーマに指定した名前でテーブルを作成する
-          return knex.schema.createTable('テーブル名', 
+          return knex.schema.createTable('snack', 
             // 作成したテーブルにカラムを作成する
             function(table) {
                // テーブルの要素設定 別途記載
               table.increments('id').primary();
               table.string('name', 100);
+              table.string('kana', 100);
+              table.string('maker', 100);
               table.integer('price');
+              table.string('type', 100);
+              table.string('regist', 100);
+              table.string('url', 1000);
+              table.string('area', 100);
+              table.string('tags', 1000);
+              table.string('image', 1000);
+              table.string('comment', 1000);
           });
       }else{
           return new Error("The table already exists. 2");
@@ -26,10 +35,10 @@ exports.up = function(knex) {
  * @returns { Promise<void> }
  */
 exports.down = function(knex) {
-    return knex.schema.hasTable('テーブル名').then(function(exists) {
+    return knex.schema.hasTable('snack').then(function(exists) {
         if (exists) {
             // 指定したテーブルを削除する
-            return knex.schema.dropTable('テーブル名');
+            return knex.schema.dropTable('snack');
         }
     }); 
 };
--- knexfile.js
+++ knexfile.js
@@ -16,6 +16,9 @@ module.exports = {
     migrations: {
       directory:'./db/migrations',
       tableName: 'knex_migrations'
+    },
+    seeds: {
+      directory: "./db/seeds",
     }
   },
 
